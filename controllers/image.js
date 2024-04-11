90f1df51994f33355c3a6aed15635be293a8fe41
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-04-11 17:15:04 +0200

    smart_brain_api

---
+++ controllers/image.js
@@ -0,0 +1,53 @@
+const axios = require('axios')
+
+const handleApiCall = (req, res) => {
+    const PAT = '7170af8c418b4968b80384009d6df2a5'; 
+    const IMAGE_URL = req.body.input;
+    const raw = {
+        "user_app_id": {
+            "user_id": 'clarifai',
+            "app_id": 'main'
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    };
+
+    axios.post("https://api.clarifai.com/v2/models/face-detection/versions/45fb9a671625463fa646c3523a3087d5/outputs", 
+                raw,
+                { 
+                    headers: {
+                        'Accept': 'application/json',
+                        'Authorization': 'Key ' + PAT
+                    }
+                }
+            )
+        .then(response => {
+            res.json(response.data)
+
+        })
+        .catch(err => res.status(400).json('unable to work with api'))
+}
+
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0].entries);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleApiCall,
+    handleImage
+}
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const knex = require('knex');
+
+// Controllers
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+// Viene istanziato un'istanza di Knex per interagire con il database PostgreSQL. 
+// La configurazione include i dettagli di connessione al database come host, porta, 
+// nome utente, password e nome del database.
+const db = knex({
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      port: 5432,
+      user: 'postgres',
+      password: 'test',
+      database: 'smart-brain',
+    },
+});
+
+//Viene creato un'istanza di un'app Express.
+//Root
+const app = express();
+
+
+//analizza il corpo della richiesta HTTP, che potrebbe contenere dati in formato JSON,
+//e li trasforma in oggetti JavaScript che possono essere manipolati all'interno dell'applicazione Node.js.
+app.use(bodyParser.json());
+
+// Il middleware CORS gestisce le intestazioni CORS nelle risposte del server, 
+// consentendo al client di accedere alle risorse del server 
+// da una diversa origine senza incorrere in problemi di sicurezza del browser.
+app.use(cors());
+
+
+// Definiamo ogni route per ogni richiesta, req: richiesta HTTP in arrivo dal client al server
+// res: risposta HTTP che il server invia al client in risposta alla richiesta.
+// db: database, bcrypt per la cittografia passwords.
+app.get('/', (req, res) => {
+    res.send('success');
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App is running on ${process.env.PORT}`)
+})
